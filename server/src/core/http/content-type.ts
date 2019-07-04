e4cd018a382f42a14f536061e2a90e902b631a03
caglaralkiss <caglaralkiss@gmail.com>
2019-07-05 01:00:18 +0300

    feat: add content type enums

---
+++ server/src/core/http/content-type.ts
@@ -0,0 +1,12 @@
+/**
+ * Enumerated Content-Type headers which is used in the Music Web App.
+ *
+ * @author Caglar Alkis
+ */
+
+export enum ContentType {
+    MULTIPART_FORM_DATA = 'multipart/form-data',
+    APPLICATION_JSON = 'application/json',
+    TEXT_PLAIN = 'text/plain',
+    AUDIO_MPEG = 'audio/mpeg',
+}
