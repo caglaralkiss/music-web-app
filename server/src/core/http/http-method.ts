2d00f5ae2e7d315d619a89cff6cb5202fbc058e0
caglaralkiss <caglaralkiss@gmail.com>
2019-07-10 04:11:29 +0300

    feat: add index file to http directory
    * add http-method enum

---
+++ server/src/core/http/http-method.ts
@@ -0,0 +1,11 @@
+/**
+ * Enumerated Http methods
+ */
+
+export enum HttpMethod {
+    GET = 'get',
+    POST = 'post',
+    PUT = 'put',
+    OPTIONS = 'options',
+    PATCH = 'patch'
+}
---
+++ server/src/core/http/index.ts
@@ -0,0 +1,4 @@
+export * from './app-request';
+export * from './content-type';
+export * from './http-method';
+export * from './parser/index';
