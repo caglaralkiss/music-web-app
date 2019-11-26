974f2a8bedb5a4c34b124299263329a6a56fe879
caglaralkiss <caglaralkiss@gmail.com>
2019-11-27 00:23:40 +0300

    feat: implement Home component
    * add infinite scrolling
    * add translations

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,20 +1,88 @@
 <template lang="pug">
-  div Home works
+  .home
+    .home__header
+      .home__header__main {{ $t('songs.title') }}
+      .home__header__sub {{ $t('songs.subTitle') }}
+    .home__cards
+      song-card(v-for="song in songs" :song="song")
 </template>
 
 <script lang="ts">
 
 import { Vue, Component } from 'vue-property-decorator'
+import SongCard from '@/components/shared/SongCard.vue'
+import { Page, Song } from '@/interfaces'
+import * as SongApi from '@/api/song'
 
 @Component({
   components: {
+    SongCard
   }
 })
 export default class Home extends Vue {
+  songs: Array<Song> = []
+  page: Page = {
+    pageNum: 1,
+    offset: 10
+  }
+  allDataCompleted = false
+
+  mounted() {
+    this.$el.addEventListener('scroll', async () => {
+      if (this.$el.scrollTop + this.$el.clientHeight >= this.$el.scrollHeight) {
+        if (!this.allDataCompleted) {
+          this.loadSongs()
+        }
+      }
+    })
+    this.loadSongs()
+  }
+
+  async loadSongs() {
+    try {
+      const response = await SongApi.getSongs({ ...this.page })
+      const { result, next } = response.data
+
+      this.songs.push(...result)
+
+      if (!next) {
+        this.allDataCompleted = true
+        return
+      }
+
+      this.nextPage()
+    } catch (e) {
+      this.$message.error(e)
+    }
+  }
+
+  nextPage(): void {
+    this.page.pageNum++
+  }
 }
 
 </script>
 
 <style lang="stylus" scoped>
+  .home
+    padding 10px
+    height 90%
+    width 100%
+    overflow-y scroll
 
+    &__header
+      margin 2rem
+
+      &__main
+        font-size 2.5rem
+        color $color-white
+
+      &__sub
+        font-size 1.5rem
+        color $color-grey-light-2
+
+    &__cards
+      display flex
+      flex-direction row
+      flex-wrap wrap
 </style>
