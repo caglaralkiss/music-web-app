cc187c3959706fd7146addea819410140043c26d
caglaralkiss <caglaralkiss@gmail.com>
2020-07-27 04:45:37 +0300

    feat: add auth module to Store
    * add initStore mutation in order to sync localStorage with Store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -3,16 +3,33 @@ import Vuex, { StoreOptions } from 'vuex'
 import { RootState } from '@/store/types'
 import { player } from '@/store/player'
 import { modal } from '@/store/modal'
+import { auth } from '@/store/auth'
+import { version } from '../../package.json'
 
 Vue.use(Vuex)
 
 const store: StoreOptions<RootState> = {
   state: {
-    language: 'en'
+    language: 'en',
+    version
   },
   modules: {
     player,
-    modal
+    modal,
+    auth
+  },
+  mutations: {
+    initStore(state) {
+      if (localStorage.getItem('store')) {
+        let store = JSON.parse(localStorage.getItem('store')!)
+
+        if (store.version === version) {
+          this.replaceState(Object.assign(store, state))
+        } else {
+          state.version = version
+        }
+      }
+    }
   }
 }
 
